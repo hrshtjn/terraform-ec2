cedde5297e91bb0e5766421074aade196e9132d1
harshitjain3 <hrsht.jn@gmail.com>
2023-02-08 17:52:46 -0600

    added ec2 instance tags

--- instance.tf
+++ instance.tf
@@ -1,9 +1,17 @@
 resource "aws_instance" "myinstance1" {
     ami = var.AMIS[var.AWS_REGION]
     instance_type = "t2.micro"
+
+    tags = {
+    Name = "TF_instance1"
+  }
 }
 
 resource "aws_instance" "myinstance2" {
     ami = var.AMIS[var.AWS_REGION]
     instance_type = "t2.micro"
+
+    tags = {
+    Name = "TF_instance2"
+  }
 }
